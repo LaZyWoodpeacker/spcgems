0ddedb3c89d625921a4d5cb101dd13cc190e09f5
lazywoodpeacker <lazywoodpeacker@gmail.com>
2020-05-27 19:38:05 +0500

    moves

--- src/logic.js
+++ src/logic.js
@@ -5,77 +5,71 @@ export const mock3x3 = [
 ]
 export const mock5x5 = [
     [1, 0, 1, 1, 0, 0, 0],
-    [1, 1, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 1, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0]
 ]
 
-export function test(mock, fn = (hor, s, c, t) => console.log({ hor, s, c, t })) {
-    let payload = []
-    const height = mock.length
-    const weight = mock[0].length
-    //horisontals
-    for (let y = 0; y < height; y++) {
+function testFild(mock, fn = (ob) => console.log) {
+    const payload = []
+    for (let y = fromY; y < toY; y++) {
+        let fr = cords(fromX, y)
         let count = 1
-        for (let x = 1; x < weight; x++) {
-            if (mock[y][x - 1] == mock[y][x]) {
+        for (let x = fromX + 1; x < toX; x++) {
+            let frto = cords(x, y)
+            if (fr == frto) {
                 count++
             }
             else {
                 if (count > 2) {
-                    // payload.push({ y, x: x - count, x2: x })
-                    fn(true, x - count, count, mock[y][x])
+                    fn({ count, t: fr })
                 }
+                fr = frto
                 count = 1
             }
         }
         if (count > 2) {
-            // payload.push({ y, x: weight - count, x2: weight })
-            fn(true, weight - count, count, mock[y][weight - 1])
+            fn({ count, t: fr })
         }
     }
-    //verticles
-    for (let x = 0; x < weight; x++) {
+    for (let x = fromX; x < toX; x++) {
+        let fr = cords(x, fromY)
         let count = 1
-        for (let y = 1; y < height; y++) {
-            if (mock[y - 1][x] == mock[y][x]) {
+        for (let y = fromY + 1; y < toY; y++) {
+            let frto = cords(x, y)
+            if (fr == frto) {
                 count++
             }
             else {
                 if (count > 2) {
-                    // console.log({ x, y, ys: y - count, count })
-                    // payload.push({ x, y, ys: y - count })
-                    fn(false, y - count, count, mock[y][x])
+                    fn({ count, t: fr })
                 }
+                fr = frto
                 count = 1
             }
         }
         if (count > 2) {
-            // console.log({ x, y: height, ys: height - count, count })
-            // payload.push({ x, y: height, ys: height - count })
-            fn(true, height - count, count, mock[height - 1][x])
+            fn({ count, t: fr })
         }
     }
-    return payload;
+    return payload.filter(e => e.t != 0)
 }
 
 function testMove(mock, fromX, fromY, toX, toY, x1, y1, x2, y2) {
+    const cords = (x, y) => {
+        if (x == x1 && y == y1) return mock[y2][x2];
+        if (x == x2 && y == y2) return mock[y1][x1];
+        return mock[y][x]
+    }
     const payload = []
     for (let y = fromY; y < toY; y++) {
-        let fr = mock[y][fromX]
+        let fr = cords(fromX, y)
         let count = 1
-        debugger
         for (let x = fromX + 1; x < toX; x++) {
-            let frto = mock[y][x]
-            debugger
-            if ((x == x2 || x == x1) && (y == y2 || y == y1)) {
-                frto = mock[y1][x1]
-                fr = mock[y2][x2]
-            }
-            debugger
+            let frto = cords(x, y)
             if (fr == frto) {
                 count++
             }
@@ -91,7 +85,27 @@ function testMove(mock, fromX, fromY, toX, toY, x1, y1, x2, y2) {
             payload.push({ count, t: fr })
         }
     }
-    return payload.filter(e => e.t != 0)
+    for (let x = fromX; x < toX; x++) {
+        let fr = cords(x, fromY)
+        let count = 1
+        for (let y = fromY + 1; y < toY; y++) {
+            let frto = cords(x, y)
+            if (fr == frto) {
+                count++
+            }
+            else {
+                if (count > 2) {
+                    payload.push({ count, t: fr })
+                }
+                fr = frto
+                count = 1
+            }
+        }
+        if (count > 2) {
+            payload.push({ count, t: fr })
+        }
+    }
+    return (payload.filter(e => e.t != 0).length > 0)
 }
 
 export function makeFild(mock, makeobj = () => 1) {
@@ -121,23 +135,23 @@ export function testMoves(mock, x, y, fn = (type, x, y, c) => console.log) {
         up: y > 0,
         down: y < height - 1
     }
-    const gems = []
-    for (let y = fromY; y < toY; y++)
-        for (let x = fromX; x < toX; x++) {
-            gems.push([x, y])
-        }
+    // const gems = []
+    // for (let y = fromY; y < toY; y++)
+    //     for (let x = fromX; x < toX; x++) {
+    //         gems.push([x, y])
+    //     }
 
     if (moves.right) {
-        console.log(testMove(mock, fromX, fromY, toX, toY, x, y, x + 1, y))
+        moves.right = testMove(mock, fromX, fromY, toX, toY, x, y, x + 1, y)
     }
     if (moves.left) {
-        console.log(testMove(mock, fromX, fromY, toX, toY, x, y, x - 1, y))
+        moves.left = testMove(mock, fromX, fromY, toX, toY, x, y, x - 1, y)
     }
     if (moves.up) {
-        // console.log(testMove(mock, fromX, fromY, toX, toY, x, y, x, y - 1))
+        moves.up = testMove(mock, fromX, fromY, toX, toY, x, y, x, y - 1)
     }
     if (moves.down) {
-        // console.log(testMove(mock, fromX, fromY, toX, toY, x, y, x, y + 1))
+        moves.down = testMove(mock, fromX, fromY, toX, toY, x, y, x, y + 1)
     }
-    return { gems, moves }
+    return { moves }
 }
--- src/scenes.js
+++ src/scenes.js
@@ -16,6 +16,7 @@ export default class MainScene extends Phaser.Scene {
 
     create() {
         let s = this
+        s.selected = false
         const boxWidth = 50
         const colors = [0x6666ff, 0xff0000, 0x00ff00, 0x0000ff];
         this.scoreText = this.add.text(25, 12, ['0'])
@@ -29,26 +30,62 @@ export default class MainScene extends Phaser.Scene {
         this.input.on('gameobjectdown', function (pointer, obj) {
             let r = obj
             let g = this.scene
-            let moves = testMoves(mock5x5, r.data.values.x, r.data.values.y)
             g.s.forEach(e => {
-                e.o.setAlpha(1)
+                if (e.t != 0) {
+                    e.o.setAlpha(1)
+                }
+                else {
+                    e.o.setAlpha(0)
+                }
             })
-            for (let move = 0; move < moves.gems.length; move++) {
-                g.getem(moves.gems[move][0], moves.gems[move][1]).setAlpha(0.3)
+
+            if (!s.selected) {
+                s.selected = [r.data.values.x, r.data.values.y]
             }
-            if (moves.moves.right) {
-                g.getem(r.data.values.x + 1, r.data.values.y).setAlpha(0.8)
+            else {
+                if (s.selected != [r.data.values.x, r.data.values.y]) {
+                    g.scoreText.setText([r.data.values.x + ',' + r.data.values.y, s.selected])
+                    let moves = testMoves(mock5x5, s.selected[0], s.selected[1])
+                    const swap = (from, to) => {
+                        console.log(from, to)
+                    }
+                    if (moves.moves.right) {
+                        if (s.selected[0] + 1 == r.data.values.x && s.selected[1] == r.data.values.y) swap(s.selected, [r.data.values.x, r.data.values.y])
+                    }
+                    if (moves.moves.left) {
+                        if (s.selected[0] - 1 == r.data.values.x && s.selected[1] == r.data.values.y) swap(s.selected, [r.data.values.x, r.data.values.y])
+                    }
+                    if (moves.moves.up) {
+                        if (s.selected[0] == r.data.values.x && s.selected[1] - 1 == r.data.values.y) swap(s.selected, [r.data.values.x, r.data.values.y])
+                    }
+                    if (moves.moves.down) {
+                        if (s.selected[0] == r.data.values.x && s.selected[1] + 1 == r.data.values.y) swap(s.selected, [r.data.values.x, r.data.values.y])
+                    }
+                    s.selected = false;
+                }
             }
-            if (moves.moves.left) {
-                g.getem(r.data.values.x - 1, r.data.values.y).setAlpha(0.8)
+
+            if (s.selected) {
+                let moves = testMoves(mock5x5, r.data.values.x, r.data.values.y)
+                if (Object.values(moves.moves).every(e => e == false)) {
+                    s.selected = false
+                }
+                else {
+                    if (moves.moves.right) {
+                        g.getem(r.data.values.x + 1, r.data.values.y).setAlpha(0.8)
+                    }
+                    if (moves.moves.left) {
+                        g.getem(r.data.values.x - 1, r.data.values.y).setAlpha(0.8)
+                    }
+                    if (moves.moves.up) {
+                        g.getem(r.data.values.x, (r.data.values.y - 1)).setAlpha(0.8)
+                    }
+                    if (moves.moves.down) {
+                        g.getem(r.data.values.x, (r.data.values.y + 1)).setAlpha(0.8)
+                    }
+                }
             }
-            if (moves.moves.up) {
-                g.getem(r.data.values.x, (r.data.values.y - 1)).setAlpha(0.8)
-            }
-            if (moves.moves.down) {
-                g.getem(r.data.values.x, (r.data.values.y + 1)).setAlpha(0.8)
-            }
-            g.scoreText.setText([r.data.values.x + ' ' + r.data.values.y])
+            // g.scoreText.setText([r.data.values.x + ',' + r.data.values.y, s.selected])
         })
         this.s = makeFild(mock5x5, (x, y, c) => {
             let rect = this.add.rectangle(Phaser.Math.Between(-500, 500), Phaser.Math.Between(-500, 500), boxWidth - 2, boxWidth - 2, colors[c]);
@@ -72,6 +109,5 @@ export default class MainScene extends Phaser.Scene {
             });
             return rect
         })
-        // console.log(test(mock5x5))
     }
 }
